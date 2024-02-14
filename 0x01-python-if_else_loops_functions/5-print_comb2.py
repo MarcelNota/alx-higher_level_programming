530a3830a389d8ffd420e169041a40ca83389fa8
MarcelNota <n3gr1t0.mn@gmail.com>
2024-02-14 14:17:18 +0200

    5 correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-
-for i in range(0, 100):
-    if i != 99:
-        print("{:02d}, ".format(i), end="")i
-    elif i == 99:
-        print("{:02d}".format(i))
+for num in range(0, 99):
+    print('{:02d}, '.format(num), end='')
+    print('99')
